bf34809dad4592ec899f3c3bf9fed28e09fca153
Adriano Froes <42614927+AdrianoFroes@users.noreply.github.com>
2024-11-25 22:34:56 -0300

    API JOGOS
    Praticando Spring Boot

---
+++ API-DSLIT/src/main/java/com/froes/dslist/services/GameService.java
@@ -0,0 +1,39 @@
+package com.froes.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.froes.dslist.dto.GameDTO;
+import com.froes.dslist.dto.GameMinDTO;
+import com.froes.dslist.entities.Game;
+import com.froes.dslist.projections.GameMinProjection;
+import com.froes.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(@PathVariable Long listId) {
+		Game result = gameRepository.findById(listId).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		return result.stream().map(GameMinDTO::new).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByGameList(Long listId) {
+		List<GameMinProjection> games = gameRepository.searchByList(listId);
+		return games.stream().map(GameMinDTO::new).toList();
+	}
+}
